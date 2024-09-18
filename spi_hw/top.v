5d6d9ff34e6ca325396ca9cc8c69094c6bd36b53
Ed Vidal Jr <develone@sbcglobal.net>
2024-09-18 08:38:21 -0600

    testing spi & spi_hw

--- spi_hw/top.v
+++ spi_hw/top.v
@@ -6,7 +6,7 @@
 //0x06 write vector, the computer will send 4 * 32bit values
 //0x07 read vector, the fpga will send 4 * 32bit values
 
-module top(input [3:0] SW, input clk, output LED_R, output LED_G, output LED_B, input SPI_SCK, input SPI_SS, input SPI_MOSI, output SPI_MISO, input [3:0] SW);
+module top( input CLK, output LED_R, output LED_G, output LED_B, input SPI_SCK, input SPI_SS, input SPI_MOSI, output SPI_MISO);
 
    parameter NOP=0, INIT=1, WR_INVERTED=2, WR_LEDS=4, WR_VEC=6, RD_VEC=7;
 
@@ -35,7 +35,7 @@ module top(input [3:0] SW, input clk, output LED_R, output LED_G, output LED_B, 
    wire spi_sck;
    wire spi_csn;
 
-   SB_SPI SB_SPI_inst(.SBCLKI(clk), .SBSTBI(spi_stb), .SBRWI(spi_rw),
+   SB_SPI SB_SPI_inst(.SBCLKI(CLK), .SBSTBI(spi_stb), .SBRWI(spi_rw),
       .SBADRI0(spi_adr[0]), .SBADRI1(spi_adr[1]), .SBADRI2(spi_adr[2]), .SBADRI3(spi_adr[3]), .SBADRI4(spi_adr[4]), .SBADRI5(spi_adr[5]), .SBADRI6(spi_adr[6]), .SBADRI7(spi_adr[7]),
       .SBDATI0(spi_dati[0]), .SBDATI1(spi_dati[1]), .SBDATI2(spi_dati[2]), .SBDATI3(spi_dati[3]), .SBDATI4(spi_dati[4]), .SBDATI5(spi_dati[5]), .SBDATI6(spi_dati[6]), .SBDATI7(spi_dati[7]),
       .SBDATO0(spi_dato[0]), .SBDATO1(spi_dato[1]), .SBDATO2(spi_dato[2]), .SBDATO3(spi_dato[3]), .SBDATO4(spi_dato[4]), .SBDATO5(spi_dato[5]), .SBDATO6(spi_dato[6]), .SBDATO7(spi_dato[7]),
