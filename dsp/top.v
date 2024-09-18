439cb94719af7740c69469c9728b9f8f65e6c146
Ed Vidal Jr <develone@sbcglobal.net>
2024-09-18 11:14:02 -0600

    missing chgs

--- dsp/top.v
+++ dsp/top.v
@@ -1,18 +1,18 @@
 `include "calc.v"
 `include "calc_dsp.v"
 
-module top(input [3:0] SW, input clk, output LED_R, output LED_G, output LED_B);
+module top( input CLK, output LED_R, output LED_G, output LED_B);
 
    wire correct;
 
    //implementation without DSP
    // calc calc_inst(
-   //    .clk(clk), .correct(correct)
+   //    .clk(CLK), .correct(correct)
    // );
 
    //implementation with DSP
    calc_dsp calc_dsp_inst(
-      .clk(clk), .correct(correct)
+      .clk(CLK), .correct(correct)
    );
 
   //leds are active low
@@ -23,7 +23,7 @@ module top(input [3:0] SW, input clk, output LED_R, output LED_G, output LED_B);
   initial begin
   end
 
-  always @(posedge clk)
+  always @(posedge CLK)
   begin
   end
 
